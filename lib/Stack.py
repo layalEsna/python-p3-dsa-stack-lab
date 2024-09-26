d9c3df3896040aad7d99abb61cfd2fc6653bf4ef
Amene <layla_1380@yahoo.com>
2024-09-25 21:26:45 -0500

    stack functions

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,58 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+       self.items = items
+       self.limit = limit
 
     def isEmpty(self):
-        pass
+        if len(self.items)== 0:
+            return True
+        else:
+            return False
 
     def push(self, item):
-        pass
+        if len(self.items) < self.limit:
+            self.items.append(item)
+            return self.items
+        else:
+            return 'Stack Overflow'
 
     def pop(self):
-        pass
+        if len(self.items) > 0:
+          return self.items.pop()
+        else:
+            return IndexError('Pop from empty stack')
 
     def peek(self):
-        pass
+        if len(self.items) > 0:
+           return self.items[-1]
+        else:
+            return None
     
     def size(self):
-        pass
+        if len(self.items) > 0:
+         return len(self.items)
 
     def full(self):
-        pass
+        if len(self.item) > 0:
+            return 'Full'
 
     def search(self, target):
-        pass
+        for index, item in enumerate(reversed(self.items)):
+            if item == target:
+                return index
+           
+        return -1
+    def full(self):
+      return len(self.items) >= self.limit
+
+    # def empty(self):
+    #     if len(self.items) == 0:
+    #         return True
+    #     else: 
+    #         return False
+    def empty(self):
+        if len(self.items) == 0:
+            return 1 
+        else:
+            return 0  
